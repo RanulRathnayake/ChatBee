a8a8b139e9da53956b54a97ac1ed2be613b2337e
RanulRathnayake <rathnayakerg.20@uom.lk>
2025-11-23 07:50:03 +0530

    cors urls commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://chat-bee.vercel.app'    
+      'https://chat-bee-one.vercel.app/login'    
     ],
     credentials: true,
   });
